09833372ab1af0d4aeb447d62c7c82a7a878a39a
Furkan <simsekfurkann07@gmail.com>
2022-06-12 22:24:14 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { btns } from "./btns";
 
-function App() {
+const App = () => {
+  const [operator, setOperator] = useState<null | string>(null);
+  const [result, setResult] = useState(0);
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  const tap = (_text: any) => {
+    if (
+      _text === "÷" ||
+      _text === "+" ||
+      _text === "%" ||
+      _text === "-" ||
+      _text === "x"
+    ) {
+      setOperator(_text);
+    } else if (true) {
+      if (operator === null) {
+        const _cache = num1 * 10;
+        const text = _cache + _text;
+        setNum1(text);
+        setResult(text);
+      } else {
+        const _cache = _text * 10;
+        const text = num2 + _text;
+        setNum2(text);
+        setResult(text);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="result">{result}</div>
+      <div className="btns">
+        {btns.map((item) => {
+          return (
+            <div
+              key={`Item ${item.text}`}
+              onClick={() => tap(item.text)}
+              className={`btn ${item.text} ${item.color} ${item.size}`}
+            >
+              {item.text}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/btns.ts
@@ -0,0 +1,23 @@
+const btns = [
+  { text: "AC" },
+  { text: "+/-" },
+  { text: "%" },
+  { text: "÷", color: "orange" },
+  { text: 7 },
+  { text: 8 },
+  { text: 9 },
+  { text: "x", color: "orange" },
+  { text: 4 },
+  { text: 5 },
+  { text: 6 },
+  { text: "-", color: "orange" },
+  { text: 1 },
+  { text: 2 },
+  { text: 3 },
+  { text: "+", color: "orange" },
+  { text: 0, size: "large" },
+  { text: "." },
+  { text: "=", color: "orange" },
+];
+
+export { btns };
