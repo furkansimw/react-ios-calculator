23cfc663a3b7ef0e0d7762aef94c95aa0bb49072
Furkan <simsekfurkann07@gmail.com>
2022-06-13 20:38:54 +0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,11 @@ const App = () => {
   const [result, setResult] = useState(0);
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
-
-  const tap = (_text: any) => {
-    if (is(_text, ["+", "-", "-", "x", "÷", "%"])) {
-      setOperator(_text);
-    } else if (is(_text, ["="])) {
-      if (operator === null) return;
-      else {
-        work();
-      }
-    } else {
-      if (operator === null) {
-        const number = num1 * 10 + _text;
-        setNum1(number);
-        setResult(number);
-      } else {
-        const number = num2 * 10 + _text;
-        setNum2(number);
-        setResult(number);
-      }
+  const is = (_text: any, contains: string[]): boolean => {
+    if (contains.includes(_text)) {
+      return true;
     }
+    return false;
   };
   const work = () => {
     if (operator === "+") {
@@ -44,12 +29,36 @@ const App = () => {
       setResult(num1 % num2);
     }
   };
-  const is = (_text: any, contains: string[]): boolean => {
-    if (contains.includes(_text)) {
-      return true;
+  const tap = (_text: any) => {
+    if (is(_text, ["+", "-", "-", "x", "÷", "%"])) {
+      if (result !== null) {
+        setNum2(0)
+        setNum1(result);
+      }
+      setOperator(_text);
+    } else if (is(_text, ["AC"])) {
+      setResult(0);
+      setNum1(0);
+      setNum2(0);
+      setOperator(null);
+    } else if (is(_text, ["="])) {
+      if (operator === null) return;
+      else {
+        work();
+      }
+    } else {
+      if (operator === null) {
+        const number = num1 * 10 + _text;
+        setNum1(number);
+        setResult(number);
+      } else {
+        const number = num2 * 10 + _text;
+        setNum2(number);
+        setResult(number);
+      }
     }
-    return false;
   };
+
   return (
     <div className="App">
       <div className="result">{result}</div>
